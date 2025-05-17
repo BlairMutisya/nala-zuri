664ff84e526a41d3fe2d343b6ff73a37532a8367
BlairMutisya <tonnyblair09@gmail.com>
2025-05-17 11:11:48 +0300

    update the app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-
-// import Footer from "./components/Footer";
-import Inquiries from "./pages/Inquiries";
 import LandingPage from "./pages/LandingPage";
+import HowToExplore from './pages/How';
+import MakeADifference from "./pages/MakeADifference";
+import OurStory from "./pages/OurStory";
+import WhatsAppChat from "./components/WhatsAppChat";
+import Inquiries from "./pages/Inquiries";
 import WhenAndWhere from "./pages/WhenAndWhere";
 import Amboseli from "./pages/Amboseli";
 import Tsavo from "./pages/Tsavo National Parks"; // Import the Tsavo component
@@ -21,7 +22,6 @@ import Watamu from "./pages/Watamu"; // Import the Watamu component
 import Kilifi from "./pages/Kilifi"; // Import the Kilifi component
 import Diani from "./pages/Diani"; // Import the Diani component
 
-
 function App() {
   return (
     <div className="app">
@@ -30,8 +30,25 @@ function App() {
       <Routes>
         <Route path="/Inquiries" element={<Inquiries />} />
         <Route path="/" element={<LandingPage />} />
-
-    
+        <Route path="/how" element={<HowToExplore />} />
+        <Route path="/makeadifference" element={<MakeADifference />} />
+        <Route path="/ourstory" element={<OurStory />} />
+        <Route path="/when-and-where" element={<WhenAndWhere />} />
+        <Route path="/amboseli" element={<Amboseli />} />
+        <Route path="/tsavo" element={<Tsavo />} />
+        <Route path="/nakuru" element={<LakeNakuru />} />
+        <Route path="/samburu" element={<Samburu />} />
+        <Route path="/nairobi" element={<Nairobi />} />
+        <Route path="/meru" element={<Meru />} />
+        <Route path="/olpejeta" element={<OlPejeta />} />
+        <Route path="/maasai-mara" element={<MaasaiMara />} />
+        <Route path="/lamu" element={<Lamu />} />
+        <Route path="/watamu" element={<Watamu />} />
+        <Route path="/kilifi" element={<Kilifi />} />
+        <Route path="/diani" element={<Diani />} /> 
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
+        {/* <Route path="/tickets" element={<TicketsPage />} /> */}
       </Routes>
     </div>
   );
